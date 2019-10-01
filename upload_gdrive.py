145cecb2c9a620cbfd354d12f89bf7107e4c83bc
Peter <peter.jung@tu-berlin.de>
2019-10-01 10:33:46 +0200

    added simple gdrive upload routine

--- upload_gdrive.py
+++ upload_gdrive.py
@@ -1,15 +1,4 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-from google.colab import auth
-from oauth2client.client import GoogleCredentials
-
-# 1. Authenticate and create the PyDrive client.
-auth.authenticate_user()
-gauth = GoogleAuth()
-gauth.credentials = GoogleCredentials.get_application_default()
-drive = GoogleDrive(gauth)  
-
-# get the folder id where you want to save your file
-file = drive.CreateFile({'parents':[{u'id': folder_id}]})
-file.SetContentFile('LAMP_bg_giid.npz')
-file.Upload() 
+from google.colab import drive
+drive.mount('gdrive' , force_remount=True)
+!mkdir -p 'gdrive/My Drive/colab/lamp'
+!cp LAMP_bg_giid.npz 'gdrive/My Drive/colab/lamp'
